be2b2d507720f9b59e2c11bb8fc11be6d055907b
joelbrewster <hi@joelbrewster.com>
2018-07-22 00:06:15 +1000

    Cleaning up styles

--- source/app.js
+++ source/app.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
-  // Fade the body in
-  $("body").addClass("visible")
+	// Fade the body in
+	$('body').addClass('visible');
 
-  // Menu system
-  ;(function() {
-    $(".menu-wrapper").on("click", function() {
-      $(".hamburger-menu").toggleClass("animate");
-      // Expand the overlay. Was it fullscreen?
-      $("body").toggleClass("no-scroll");
-      $(".navigation-menu").toggleClass("visible");
-    })
-  })()
-  //Masonry layout
-  // $(".grid").imagesLoaded(function () {
-  //     $(".grid").masonry({
-  //         itemSelector: ".grid-item"
-  //     });
-  // });
-  // console.log("loaded")
+	// Menu system
+	(function() {
+		$('.menu-wrapper.right').on('click', function() {
+			$('.hamburger-menu').toggleClass('animate')
+			$('body').toggleClass('no-scroll')
+			$('.navigation-menu').toggleClass('visible')
+		})
+	})()
 })
