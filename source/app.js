fb9193af25497e1d3c72662918a73dd31c5bf002
Joel Brewster <hi@joelbrewster.com>
2018-09-16 18:28:15 +1000

    uploadshizzle

--- source/app.js
+++ source/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-	// Fade the body in
-	$('body').addClass('visible');
+    // Fade the body in
+    $('body').addClass('visible');
 
-	// Menu system
-	(function() {
-		$('.menu-wrapper.right').on('click', function() {
-			$('.hamburger-menu').toggleClass('animate')
-			$('body').toggleClass('no-scroll')
-			$('.navigation-menu').toggleClass('visible')
-		})
-	})()
-})
+    // Menu system
+    (function() {
+        $('.menu-wrapper.right').on('click', function() {
+            $('.hamburger-menu').toggleClass('animate');
+            $('body').toggleClass('no-scroll');
+            $('body').toggleClass('transform');
+            $('.navigation-menu').toggleClass('visible');
+        });
+    })();
+});
