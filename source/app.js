18a0753ce4bae9ed004829afc58143c1c97387db
Joel Brewster <joel@blstr.co>
2018-09-26 21:53:51 +1000

    fixerator

--- source/app.js
+++ source/app.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-    // Fade the body in
-    $('body').addClass('visible');
+// $(document).ready(function() {
+//     // Fade the body in
+//     $('body').addClass('visible');
 
-    // Menu system
-    (function() {
-        $('.menu-wrapper.right').on('click', function() {
-            $('.hamburger-menu').toggleClass('animate');
-            $('body').toggleClass('no-scroll');
-            $('body').toggleClass('transform');
-            $('.navigation-menu').toggleClass('visible');
-        });
-    })();
-});
+//     // Menu system
+//     (function() {
+//         $('.menu-wrapper.right').on('click', function() {
+//             $('.hamburger-menu').toggleClass('animate');
+//             $('body').toggleClass('no-scroll');
+//             $('body').toggleClass('transform');
+//             $('.navigation-menu').toggleClass('visible');
+//         });
+//     })();
+// });
